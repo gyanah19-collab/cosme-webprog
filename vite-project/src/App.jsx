c802cdd04dd5eb9a44ca799134691e540c621b62
unknown <gyanah19@gmail.com>
2026-03-11 14:08:11 +0800

    lab-act1

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="App">
+      <header className="App-Header">
+        <h1>Welcome to My React App!</h1>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Name: Gilianne Cosme <br />
+          Email: gyanah19@gmail.com <br />
+          Age: 20 <br />
+          Course: BSIT MWA <br />
+          Section: INF231 <br />
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
